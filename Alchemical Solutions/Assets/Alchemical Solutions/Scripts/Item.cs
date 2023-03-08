d46d936e9000b4926fcc8f3ed775e49818575aa3
zyrex-devs <72226082+zy-rex@users.noreply.github.com>
2023-03-08 11:49:58 +0000

    Inventory work
    21:04

--- Alchemical Solutions/Assets/Alchemical Solutions/Scripts/Item.cs
+++ Alchemical Solutions/Assets/Alchemical Solutions/Scripts/Item.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[Serializable]
 public class Item
 {
     public enum ItemType
@@ -9,6 +12,7 @@ public class Item
         HeatPotion,
         FrostPotion,
         PoisonPotion,
+        Trimmers,
     }
 
     public ItemType itemType;
@@ -22,6 +26,18 @@ public class Item
             case ItemType.FrostPotion: return ItemAssets.Instance.frostPotionSprite;
             case ItemType.HeatPotion: return ItemAssets.Instance.heatPotionSprite;
             case ItemType.PoisonPotion: return ItemAssets.Instance.poisonPotionSprite;
+        }
+    }
+
+    public bool IsStackable()
+    {
+        switch (itemType)
+        {
+            default:
+            case ItemType.HeatPotion:
+            case ItemType.FrostPotion:
+            case ItemType.PoisonPotion:
+                return true;
         }
     }
 }
